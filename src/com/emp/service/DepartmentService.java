e4f3bc77e86b28410a6ddf3440e8e7d484db4fe0
abbas886 <abbas886@yahoo.com>
2018-05-26 09:38:35 +0530

    naming conventions + unused methods

--- src/com/emp/service/DepartmentService.java
+++ src/com/emp/service/DepartmentService.java
@@ -9,17 +9,21 @@ public interface DepartmentService {
 public boolean add(Department department);
 public boolean update(Department department);
 public boolean delete(int departmentId);
+  //use camel case
 public Department getdep(int departmentId);
+  //what is this method doing?  why return type is void?
 public void getAllDepartmentName();
 public void display(Department dep);
 //public boolean add(Employee emp, Department department);
 //public boolean add(int empid,int departmentId); 
 public boolean addEmployeeToDepartment(int empId, int deptId);
+    //use camel case
 public boolean deleteEmployeefromDepartment(int deptId,int empId);
 public List<Department> getdepartment();
 public List<Employee> getEmployees(int DeptId);
 public List<Employee> getEmployees(int deptId,int amount);
 public List<Employee> getEmployees(int deptId,int min,int max);
+  //what is access specifier?
 void display();
 
 }
