2c9a2b5d5a6b37ef3ff40a70aac3c5c4231681fe
abbas886 <abbas886@yahoo.com>
2018-05-26 09:36:26 +0530

    //1)Why EmployeeList class??

--- src/com/emp/model/EmployeeList.java
+++ src/com/emp/model/EmployeeList.java
@@ -1,7 +1,9 @@
 package com.emp.model;
 
 import java.util.List;
-
+//1)Why EmployeeList class??
+//2)display method you can write in impl class
+//3)why so many classes in model package?  There should be only two model classes.
 public class EmployeeList {
 public void display(List<EmployeeDetails> empList){
 	for(EmployeeDetails e:empList){
