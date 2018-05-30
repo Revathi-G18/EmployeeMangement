1404d01ec507b3d3ded0993288e60a87313d092b
Revathi-G18 <revathigunasekaran1869@gmail.com>
2018-05-30 10:00:14 +0530

    UI

--- src/com/emp/db/DBConnectionTest.java
+++
@@ -1,9 +0,0 @@
-package com.emp.db;
-
-public class DBConnectionTest {
-public static void main(String[] args) {
-	if(DBConnection.getConnection()!=null){
-		System.out.println("Connection Established...");
-	}
-}
-}
